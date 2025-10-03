4f1fb8137dc192e51b9436c4352821832c27a119
Fernando Lugo <21302053@utfv.edu.mx>
2025-10-03 09:03:10 -0600

    Diseño Login

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicia Sesión</title>
+  <link rel="stylesheet" href="/src/css/login.css">
+</head>
+<body>
+  <div class="left">
+    <img src="/src/assets/img/sistemasLogo.jpeg" alt="Logo C.A.S Sistemas">
+  </div>
+
+  <div class="right">
+    <div class="login-box">
+      <h2>Iniciar Sesión</h2>
+      <form action="#">
+        <input type="text" placeholder="Usuario">
+        <input type="password" placeholder="Contraseña">
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
