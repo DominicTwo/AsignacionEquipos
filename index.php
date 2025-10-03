a69d7ee9cb3397dabe6ebfa29f3673bc972f1354
Dominic Contreras <22102006@utfv.edu.mx>
2025-10-03 09:42:48 -0600

    First Commit

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       <form action="#">
         <input type="text" placeholder="Usuario">
         <input type="password" placeholder="Contraseña">
-        <button type="submit">Entrar</button>
+        <button type="submit">Entrar :D</button>
       </form>
     </div>
   </div>
