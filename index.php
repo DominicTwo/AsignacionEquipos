321d8795ddaddc89d0e0e07f9ff03f33fd40caa5
Fernando Lugo <21302053@utfv.edu.mx>
2025-10-06 08:53:50 -0600

    Heasder

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inicia Sesión</title>
-  <link rel="stylesheet" href="/src/css/login.css">
-</head>
-<body>
-  <div class="left">
-    <img src="/src/assets/img/sistemasLogo.jpeg" alt="Logo C.A.S Sistemas">
-  </div>
-
-  <div class="right">
-    <div class="login-box">
-      <h2>Iniciar Sesión</h2>
-      <form action="#">
-        <input type="text" placeholder="Usuario">
-        <input type="password" placeholder="Contraseña">
-        <button type="submit">Entrar</button>
-      </form>
+<?php 
+  $titlePage = 'Iniciar Sesión';
+  require __DIR__ . '/src/templates/HeaderHTML.php';
+?>
+    <div class="left">
+      <img src="/src/assets/img/sistemasLogo.jpeg" alt="Logo C.A.S Sistemas">
     </div>
-  </div>
-</body>
+
+    <div class="right">
+      <div class="login-box">
+        <h2>Iniciar Sesión</h2>
+        <form action="login.php" method="POST">
+          <input type="text" id="usuario" name="usuario" placeholder="Usuario" onkeyup="validarInputs()">
+          <input type="password" id="password" name="password" placeholder="Contraseña" onkeyup="validarInputs()">
+          <button type="submit" id="btnLogin" disabled>Entrar</button>
+        </form>
+      </div>
+    </div>
+
+      <script>
+    function validarInputs() {
+      const user = document.getElementById("usuario").value.trim();
+      const pass = document.getElementById("password").value.trim();
+      document.getElementById("btnLogin").disabled = (user === "" || pass === "");
+    }
+  </script>
+  </body>
 </html>
