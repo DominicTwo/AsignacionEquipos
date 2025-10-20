6b8265d19ae89e46da5e3a69256bd5e9d4d34392
Dominic Contreras <22102006@utfv.edu.mx>
2025-10-20 08:33:44 -0600

    Nav actualizacion

--- src/templates/Navbar.php
+++ src/templates/Navbar.php
@@ -1,8 +1,8 @@
+<head>
+<link rel="stylesheet" href="../css/navbar.css">
+</head>
 <header class="header-main-sistemas">
         <nav class="nav-main-sistemas">
-            <a href="/sistemas">Sistemas</a>
-            <a href="/usuarios">Usuarios</a>
-            <a href="/roles">Roles</a>
             <a href="/sistemas">Sistemas</a>
             <a href="/usuarios">Usuarios</a>
             <a href="/roles">Roles</a>
