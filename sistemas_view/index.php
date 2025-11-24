d80101025d8173f1bc3a02dea5d91fab0de07267
Dominic Contreras <22102006@utfv.edu.mx>
2025-11-24 14:48:41 -0600

    Update Nav

--- sistemas_view/index.php
+++ sistemas_view/index.php
@@ -1,41 +1,41 @@
 <?php
 // require_once '../auth.php';
 // proteger_ruta(['sistemas']); protege las rutas
- require '../src/templates/NavBar.php'
+ include '../src/templates/Navbar.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="stylesheet" href="../src/css/sistemas/index.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-     <main class="main-cards-sistemas">
-     
 
-            <a href="ViewAsignaciones.php" class="link-cards">
-                <div class="card-content asignacion-card">
-                <h2 class="title-card title-asignaciones">Asignacioness</h2>
-                </div>
-            </a>
-            <a href="ViewAsignaciones.php" class="link-cards">
-                <div class="card-content cambio-card" >
-                <h2 class="title-card">Cambios</h2>
-                </div>
-            </a>
-            <a href="ViewAsignaciones.php" class="link-cards">
-                <div class="card-content cancelacion-card">
-                <h2 class="title-card">Cancelaciones</h2>
-                </div>
-            </a>
-            <a href="ViewAsignaciones.php" class="link-cards">
-                <div class="card-content bajas-card">
-                <h2 class="title-card">Bajas</h2>
-                </div>
-            </a>
-            
+<body>
+
+    <?php  ?>
+
+     <main class="main-cards-sistemas">       
+              
+        <div class="card-content asignacion-card">
+            <h2 class="title-card title-asignaciones">Asignaciones</h2>
+        </div>
+
+        <div class="card-content cambios-card">
+            <h2 class="title-card title-asignaciones">Cambios</h2>
+        </div>
+
+        <div class="card-content cancelaciones-card">
+            <h2 class="title-card title-asignaciones">Cancelaciones</h2>
+        </div>
+
+        <div class="card-content bajas-card">
+            <h2 class="title-card title-asignaciones">Bajas</h2>
+        </div>
+
     </main>       
 </body>
+
 </html>
